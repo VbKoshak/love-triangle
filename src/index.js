c4c211fa226e726a3f484d4eb57b8280e0079610
Vlad_Boltrukanis <boltrukanisvlad@gmail.com>
2018-09-12 16:32:25 +0300

    Finished work

--- src/index.js
+++ src/index.js
@@ -4,4 +4,31 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0;
+var size = preferences.length;
+for (var i = 0; i < size;i++)
+{
+    for (var j = 0; j < size; j++)
+    {
+      if (i == j)
+      {
+          continue;
+      };
+        if (preferences[i] == (j+1))
+        {
+            if (j == k || k == i)
+            {
+                continue;
+            };
+            for (var k = 0; k < size; k++)
+            {
+              if ((preferences[j] == (k+1)) && preferences[k] == (i+1))
+              {
+                  count++;
+              };
+            };
+        };
+    };
+};
+return count/3;
 };
